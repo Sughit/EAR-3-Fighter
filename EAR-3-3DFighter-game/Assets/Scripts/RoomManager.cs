1edd6d321fe93131c7d7d567fa639419d5dd8b8f
Sughit <andreisorici2007@gmail.com>
2023-10-14 21:01:19 +0300

    prima incercare pt multiplayer

---
+++ EAR-3-3DFighter-game/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public GameObject player;
+    [Space]
+    public Transform spawnPoint;
+    
+    void Start()
+    {
+        Debug.Log("Conecting...");
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        Debug.Log("Connected to server");
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
+
+        Debug.Log("We're connected and in a room now");
+
+        GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+        _player.GetComponent<PlayerSetup>().IsLocalPlayer();
+    }
+}
