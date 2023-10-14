99c759235b428eab4be05468845ec0e5b584f3b2
emanuel5867 <emisor.2009@gmail.com>
2023-10-14 20:16:01 +0300

    miscare

---
+++ EAR-3-3DFighter-game/Assets/Movement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+   
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+        
+        if (movementDirection != Vector3.zero)
+        {
+            transform.forward = movementDirection;
+        }
+    }
+}
