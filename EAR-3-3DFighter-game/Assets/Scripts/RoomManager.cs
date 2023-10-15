3dc5dcae1faca6600169c99dcbc890633dd7dad6
Sughit <andreisorici2007@gmail.com>
2023-10-15 10:57:10 +0300

    conecting screen

--- EAR-3-3DFighter-game/Assets/Scripts/RoomManager.cs
+++ EAR-3-3DFighter-game/Assets/Scripts/RoomManager.cs
@@ -9,6 +9,8 @@ public class RoomManager : MonoBehaviourPunCallbacks
     public GameObject weaponManager;
     [Space]
     public Transform spawnPoint;
+    [Space]
+    public GameObject roomCam;
     bool playerJoined;
     public static string roomCode = "test";
     
@@ -43,6 +45,7 @@ public class RoomManager : MonoBehaviourPunCallbacks
     {
         yield return new WaitForSeconds(1f);
         Debug.Log("The player will spawn soon");
+        roomCam.SetActive(false);
         GameObject _weaponManager = PhotonNetwork.Instantiate(weaponManager.name, Vector3.zero, Quaternion.identity);
         Debug.Log("You joined "+ PhotonNetwork.CurrentRoom.Name);
     }
