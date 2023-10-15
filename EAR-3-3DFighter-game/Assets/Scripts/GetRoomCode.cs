529d5e43cb8e272b90460256bf2bb9f1c1a96667
Sughit <andreisorici2007@gmail.com>
2023-10-15 12:05:14 +0300

    gameModuri v1

--- EAR-3-3DFighter-game/Assets/Scripts/GetRoomCode.cs
+++ EAR-3-3DFighter-game/Assets/Scripts/GetRoomCode.cs
@@ -3,20 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
-public class GetRoomCode : MonoBehaviour
+public class GetRoomCode : MonoBehaviourPunCallbacks
 {
     public Text roomCode;
+    public Text joinRoomCode;
+
+    public static string gameMode;
+
     public void ManageRoomCode()
     {
         if(roomCode.text!="")
         {
+            PhotonNetwork.AutomaticallySyncScene = true;
             RoomManager.roomCode = roomCode.text;
-            SceneManager.LoadScene(1);
+            switch (gameMode)
+            {
+                case "1v1":
+                PhotonNetwork.LoadLevel("1v1");
+                break;
+                case "1v1DF":
+                PhotonNetwork.LoadLevel("1v1DF");
+                break;
+                case "wave":
+                PhotonNetwork.LoadLevel("wave");
+                break;
+                default:
+                Debug.Log("Select a game mode");
+                break;
+            }
         }
         else 
         {
             Debug.Log("Enter a room code");
         }
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.JoinOrCreateRoom(joinRoomCode.name, null, null);
+    }
+
+    public void SetGameMode(string _gameMode)
+    {
+        gameMode = _gameMode;
     }
 }
